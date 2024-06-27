1afccc5ed3e83e34aa449a929a217b6a2da2506e
Daniel Aston <daniel.aston@live.co.uk>
2024-06-27 18:19:53 +0100

    Add Header and app.scss

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,13 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/LandingPage/LandingPage";
 import FinderPage from "./pages/FinderPage/FinderPage";
 import ResultsPage from "./pages/ResultsPage/ResultsPage"
+import Header from "./components/Header/Header";
+import "./App.scss"
 
 function App() {
   return (
     <BrowserRouter>
+    <Header />
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/giftFinder" element={<FinderPage />} />
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,28 @@
-import './Header.scss';
+import { Link } from "react-router-dom";
+import "./Header.scss";
 
 export default function Header() {
-  return <></>;
+  return (
+    <section className="header">
+      <nav className="header__left">
+        <Link className="header__link-logo">
+          <img src="https://placehold.co/50" alt="" className="header__logo" />
+        </Link>
+        <Link className="header__navLink">SHOP</Link>
+        <Link className="header__navLink">DISCOVER</Link>
+        <Link className="header__navLink">HELP</Link>
+      </nav>
+      <nav className="header__right">
+        <Link className="header__link-logo">
+          <img src="https://placehold.co/30" alt="" className="header__icon" />
+        </Link>
+        <Link className="header__link-logo">
+          <img src="https://placehold.co/30" alt="" className="header__icon" />
+        </Link>
+        <Link className="header__link-logo">
+          <img src="https://placehold.co/30" alt="" className="header__icon" />
+        </Link>
+      </nav>
+    </section>
+  );
 }
