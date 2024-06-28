c04327e09a4eecd272effa08fa5da1cff1e3d4bc
Daniel Aston <daniel.aston@live.co.uk>
2024-06-28 10:09:45 +0100

    Style landing

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,5 +1,17 @@
-import './LandingPage.scss';
+import { Link } from "react-router-dom";
+import "./LandingPage.scss";
+import figure from "../../assets/landing-figure.png"
 
 export default function LandingPage() {
-  return <></>;
+  return (
+    <section className="landing">
+      <article className="landing__content">
+        <h1 className="landing__title">Looking for the perfect gift?</h1>
+        <h2 className="landing__subtitle">We’ve got you covered.</h2>
+        <Link className="landing__start">START NOW</Link>
+      </article>
+
+      <img src={figure} alt="" className="landing__figure"/>
+    </section>
+  );
 }
